0fa6998bc80bb974e0ae8703c22c72ef1f89809b
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-12-23 07:17:25 -0300

    removendo dados de uma entidade

--- src/main/java/br/com/fourcatsdev/projetowebmvc/controllers/UsuarioController.java
+++ src/main/java/br/com/fourcatsdev/projetowebmvc/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,5 +44,12 @@ public class UsuarioController {
     public String listarUsuario(Model model) {
         model.addAttribute("usuarios", usuarioRepository.findAll());
         return "/auth/admin/admin-listar-usuario";
+    }
+
+    @GetMapping("/admin/apagar/{id}")
+    public String deletarUsuario(@PathVariable("id") long id, Model model) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+        usuarioRepository.delete(usuario);
+        return "redirect:/usuario/admin/listar";
     }
 }
